11125424e529e57620956ec5613c69d4a8420093
drsuneamer <sunyeong0412@gmail.com>
2023-05-15 14:41:38 +0900

    [#COD-47][FE] feat: 헤더 레이아웃 완성

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Div = styled.div`
   width: 100%;
@@ -7,6 +10,11 @@ const Div = styled.div`
   align-items: center;
   border-bottom: solid #fab809;
   justify-content: space-between;
+
+  a {
+    text-decoration: none;
+    color: #4a483f;
+  }
 `;
 
 const Title = styled.div`
@@ -25,37 +33,106 @@ const Arena = styled.p`
 `;
 
 const Contents = styled.div`
-  width: 30%;
+  width: 40%;
+  // min-width: 500px;
   display: flex;
   justify-content: space-between;
 `;
 
 const Content = styled.div``;
 
+const CurContent = styled.div`
+  // font-weight: bold;
+  // color: #506bb1;
+`;
+
 const Login = styled.div`
   display: flex;
   margin-right: 20px;
 `;
 
+const User = styled.div`
+  display: flex;
+  margin-right: 20px;
+
+  .nickname {
+    color: #006e61;
+    font-weight: bold;
+  }
+`;
+
 function Header() {
+  const [page, setPage] = useState(useLocation().pathname);
+
+  // useEffect(() => {
+  //   setPage = page1;
+  // }, [page]);
+
+  const [login, setLogin] = useState("true"); // 로그인시 set
+  const [nickname, setNickname] = useState("성환조");
+
   return (
     <Div>
-      <Title>
-        <Code>CODE</Code>
-        <Arena>Arena</Arena>
-      </Title>
+      <Link to="/">
+        <Title>
+          <Code>CODE</Code>
+          <Arena>Arena</Arena>
+        </Title>
+      </Link>
+
       <Contents>
-        <Content>📚 기초 개념</Content>
-        <Content>📝 문제 풀이</Content>
-        <Content>📚 커뮤니티</Content>
-        <Content>🎮 단체 퀴즈</Content>
+        <Link to="/lecture">
+          {page.includes("lecture") ? (
+            <CurContent>📚 기초 개념</CurContent>
+          ) : (
+            <Content>📚 기초 개념</Content>
+          )}
+        </Link>
+
+        <a Link to="/problemSet">
+          {page.includes("problem") || page.includes("result") ? (
+            <CurContent>📝 문제 풀이</CurContent>
+          ) : (
+            <Content>📝 문제 풀이</Content>
+          )}
+        </a>
+
+        <Link to="/board/1">
+          {page.includes("board") || page.includes("article") ? (
+            <CurContent>📚 커뮤니티</CurContent>
+          ) : (
+            <Content>📚 커뮤니티</Content>
+          )}
+        </Link>
+
+        <Link to="/multiquiz">
+          {page.includes("quiz") ? (
+            <CurContent>🎮 단체 퀴즈</CurContent>
+          ) : (
+            <Content>🎮 단체 퀴즈</Content>
+          )}
+        </Link>
       </Contents>
 
-      <Login>
-        <p>로그인</p>
-        <p style={{ color: "#FAB809" }}>&nbsp;|&nbsp;</p>
-        <p>회원가입</p>
-      </Login>
+      {login ? (
+        <User>
+          <p className="nickname">{nickname}</p>
+          <p>&nbsp;님</p>
+          <p style={{ color: "#FAB809" }}>&nbsp;|&nbsp;</p>
+          <p>로그아웃</p>
+        </User>
+      ) : (
+        <Login>
+          <Link to="/login">
+            <p>로그인</p>
+          </Link>
+
+          <p style={{ color: "#FAB809" }}>&nbsp;|&nbsp;</p>
+          <Link to="/signup">
+            <p>회원가입</p>
+          </Link>
+        </Login>
+      )}
     </Div>
   );
 }
