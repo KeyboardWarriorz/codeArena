214be6e9be8258d578b0a5ef6d23f6979ee0ad68
drsuneamer <sunyeong0412@gmail.com>
2023-05-16 18:00:41 +0900

    [#COD-47][FE] feat: modal 기초 설정

--- frontend/src/pages/WordList.js
+++ frontend/src/pages/WordList.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { useState } from "react";
+import DeleteModal from "../components/DeleteModal";
 
 export default function WordList() {
-  return <div>유저 단어장</div>;
+  // 삭제 모달 관리
+  const [showModal, setShowModal] = useState(false);
+  const clickModal = () => setShowModal(!showModal);
+
+  function submit() {
+    console.log("submit");
+  }
+  return (
+    <>
+      <div onClick={clickModal}>유저 단어장</div>
+      {showModal && <DeleteModal clickModal={clickModal} func={submit} />}
+    </>
+  );
 }
