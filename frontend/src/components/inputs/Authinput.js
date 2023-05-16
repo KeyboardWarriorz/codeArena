d830c2af75a65023c68add584da45f9fda9fdb3a
drsuneamer <sunyeong0412@gmail.com>
2023-05-16 14:14:56 +0900

    [#COD-41][FE] feat: add user page layout

---
+++ frontend/src/components/inputs/Authinput.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 320px;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #d6d5d5;
+  padding-left: 8px;
+  color: #4a483f;
+  font-family: "NanumSquareNeo-Variable";
+  &: focus {
+    outline: none;
+    border: 1.9px solid #83868780;
+  }
+
+  &:: placeholder {
+    color: #bcbfc1;
+  }
+`;
+
+export default function AuthInput(props) {
+  return (
+    <div>
+      <Input type={props.type} placeholder={props.placeholder} />
+    </div>
+  );
+}
---
+++ frontend/src/components/organisms/AuthDiv.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import AuthInput from "../inputs/Authinput";
+import MiniButton from "../buttons/MiniButton";
+
+const Box = styled.div`
+  p {
+    margin-bottom: 10px;
+  }
+
+  #row1 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: default;
+  }
+
+  #row2 {
+    display: flex;
+    align-items: center;
+    cursor: default;
+    justify-content: center;
+  }
+
+  #title {
+    font-size: 14px;
+  }
+
+  #red {
+    color: #c25450;
+  }
+
+  #added {
+    margin-left: 7px;
+    font-size: 4px;
+    color: #006e61;
+    margin-bottom: 0;
+  }
+`;
+export default function AuthDiv(props) {
+  return (
+    <Box>
+      <div id="row1">
+        <div id="row2">
+          <p id="title">{props.title}</p>
+          {props.es ? <p id="red">&nbsp;*</p> : ""}
+          {props.added ? <p id="added">{props.added}</p> : ""}
+        </div>
+        {props.isbutton ? <MiniButton text="중복 확인"></MiniButton> : ""}
+      </div>
+
+      <AuthInput type={props.type} placeholder={props.placeholder} />
+    </Box>
+  );
+}
