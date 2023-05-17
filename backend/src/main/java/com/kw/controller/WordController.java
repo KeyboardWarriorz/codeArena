8e3753a9cacfb9751a976ca1bdfe4492e2ce50ca
lje71421 <lje71421@naver.com>
2023-05-17 21:13:28 +0900

    [#COD-99][BE] feat: WordController

--- backend/src/main/java/com/kw/controller/WordController.java
+++ backend/src/main/java/com/kw/controller/WordController.java
@@ -1,10 +1,10 @@
 package com.kw.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kw.entity.User;
+import com.kw.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kw.entity.UserWord;
 import com.kw.entity.Word;
@@ -12,27 +12,40 @@ import com.kw.response.responseApi;
 import com.kw.service.WordService;
 
 @RestController
+@RequiredArgsConstructor
 public class WordController {
 
-	@Autowired
-	private WordService wordService;
+	private final WordService wordService;
 
-	
+	private String searchingName; //사용자가 드래그한 단어
+	private String description; //드래그한 단어에 해당하는 설명
+
 	/**
 	 * 드래그한 단어를 DB에 저장
 	 * request: user_id, word, content
 	 * status 201 : "단어 등록 성공" / status 401 : "이미 등록된 단어" /  status 500 : "단어 등록 실패"
 	 */
 	@PostMapping("/word")
-	public String insert(UserWord userWord) {
-		
-		// 1. 사용자 User_Word DB에 같은 내용이 이미 존재하는 지 확인한다.
-		
-		// 2-1. 이미 있다면 "이미 단어사전에 등록되어 있습니다." 출력한다.
-		
-		// 2-2. 없다면 공용 Word DB에서 정보(word_id) 가져와 User_Word DB에 값을 저장한다. 
+	public ResponseEntity<responseApi<String>> insert(@RequestParam String userId) {
+		responseApi<String> response = new responseApi<String>();
 
-		return "redirect:/"; // 기존에 보고 있던 페이지에 그대로
+		// 1. 사용자 User_Word DB에 같은 내용이 이미 존재하는 지 확인한다.
+		Long now = wordService.selectByName(searchingName, userId);
+
+		// 2-1. 이미 있다면 "이미 등록된 단어" 메시지를 전달
+		if(now > 0){
+			response.setStatusCode(401);
+			response.setMessage("이미 등록된 단어");
+		}else{ // 2-2. 없다면 공용 Word DB에서 정보(word_id) 가져와 User_Word DB에 값을 저장한다.
+			Word word = wordService.selectByName(searchingName); //검색한 단어에 해당하는 word 조회
+			UserWord userWord = new UserWord(null, new User(userId), word);
+			wordService.insert(userWord);
+
+			response.setStatusCode(200);
+			response.setMessage("단어 등록 성공");
+		}
+
+		return ResponseEntity.ok(response); // 기존에 보고 있던 페이지에 그대로
 	}
 
 	/**
@@ -41,40 +54,35 @@ public class WordController {
 	 * status 200 : "단어 조회 성공" / status 500 : "단어 검색 실패"
 	 */
 	@GetMapping("/word")
-	public ResponseEntity<responseApi<String>> selectBoardAll(String name) {
+	public ResponseEntity<responseApi<String>> selectByName(String name) {
 		responseApi<String> response = new responseApi<String>();
+		searchingName = name; //드래그한 단어를 저장
 
 		// 1. 공용 Word DB에 해당하는 단어가 이미 있는 지 확인한다.
-		Word word = wordService.selectByName(name);
-	
+		Word word = wordService.selectByWordName(name);
+
 		// 2-1. 만약 없다면 ChatGPT를 통해 값을 가져온다.
 		if (word == null) {
-			String description = wordService.getWordDefinition(name);
-		
-			if(description == null) { //chatGPT에서 가져온 값이 없다면 
+			description = wordService.getWordDefinition(name);
+
+			if(description == null) { //chatGPT에서 가져온 값이 없다면
 				response.setStatusCode(500);
 				response.setMessage("단어 조회 실패");
-				response.setData(""); //null이여도 null로 보내야하는 가????????????????????????
 			} else {
 				response.setStatusCode(200);
 				response.setMessage("단어 조회 성공");
 				response.setData(description); // 단어 전송
-				
-				// 2-1-1. 공용 Word DB에 저장한다. 
-				Word saveWord = new Word();
-				saveWord.setName(name);
-				saveWord.setDescription(description);
-				
-				wordService.insert(saveWord); //저장 
+
+				// 2-1-1. 공용 Word DB에 저장한다.
+				wordService.insert(new Word(null, name, description)); //저장
 			}
 		} else { // 2-2. 이미 있다면 공용 Word DB에서 가져온 값을 가져온다.
 			response.setStatusCode(200);
 			response.setMessage("단어 조회 성공");
 			response.setData(word.getDescription()); // 단어 전송
 		}
-
 		return ResponseEntity.ok(response);
 	}
-	
+
 
 }
