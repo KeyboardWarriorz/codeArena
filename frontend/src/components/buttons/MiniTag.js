f24c3c0935b62486d5469db7cd035c6aba4d2ca6
drsuneamer <sunyeong0412@gmail.com>
2023-05-19 12:37:53 +0900

    [#COD-86][FE] feat: 게시물 세부 조회 레이아웃 (미완성)

--- frontend/src/components/buttons/MiniTag.js
+++ frontend/src/components/buttons/MiniTag.js
@@ -1,10 +1,11 @@
+import React from "react";
 import styled, { css } from "styled-components";
 
 const Tag = styled.span`
   height: 20px;
   background-color: #fab809;
   border: none;
-  border-radius: 6px;
+  border-radius: 8px;
   font-size: 14px;
   font-family: "NanumSquareNeo-Variable";
   color: white;
@@ -14,7 +15,15 @@ const Tag = styled.span`
   ${(props) =>
     props.green &&
     css`
-      background-color: green;
+      background-color: #006e61;
+    `}
+
+  ${(props) =>
+    props.red &&
+    css`
+      background-color: #c25450;
+      padding-left: 20px;
+      padding-right: 20px;
     `}
 `;
 export default function MiniTag(props) {
