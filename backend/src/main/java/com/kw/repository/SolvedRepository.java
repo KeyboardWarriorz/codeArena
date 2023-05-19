7813582205e5b5d306824f4d483e51da40969739
Chos1 <chosw0812@naver.com>
2023-05-19 14:26:15 +0900

    [#COD-78][BE] feat:맞은 문제 틀린문제 조회

--- backend/src/main/java/com/kw/repository/SolvedRepository.java
+++ backend/src/main/java/com/kw/repository/SolvedRepository.java
@@ -5,6 +5,8 @@ import com.kw.entity.User;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -12,10 +14,28 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface SolvedRepository extends JpaRepository<Solved, Integer>, QuerydslPredicateExecutor<Solved> {
 	
+	int pageNumber = 1;  // 1부터 시작하는 페이지 번호
+	int pageSize = 1;   // 페이지 크기
+	Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+	
 	List<Solved> findListByUser(User user);
 	
 	@Query(value="select s from Solved s where s.user.userId=?1 order by s.solvedId DESC")
 	List<Solved> findListByUserOrderBysolvedIdDesc(String userId);
+	
+	@Query(value="select s from Solved s where s.user.userId=?1 order by s.solvedId" ,nativeQuery = false)
+	List<Solved> findListByUserOrderBysolvedIdDesc(String userId, Pageable pageable);
+	
+	@Query(value="select s from Solved s where s.user.userId=?1 and s.problem.subcategory.category.categoryId=?2 order by s.solvedId" ,nativeQuery = false)
+	List<Solved> findListByUserOrderBysolvedIdDescWhereCate(String userId, Long categoryId, Pageable pageable);
+	
+	@Query(value="select count(s) from Solved s where s.user.userId=?1 and s.problem.subcategory.category.categoryId=?2 and s.success=?3" ,nativeQuery = false)
+	int CountSolCate(String userId, Long categoryId, int success);
+	
+	
+	@Query(value="select count(s) from Solved s where s.user.userId=?1 and s.success=?2" ,nativeQuery = false)
+	int countSolve(String userId, int success);
+	
 	
 	@Query(value="select s from Solved s where s.user.userId=?1 and s.problem.problemId=?2")
 	Solved findProAndUser(String userId, Long problemId);
