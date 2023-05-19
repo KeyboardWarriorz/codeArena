1c0264cd95882b2ccb1c05be84d1443c39f6821a
adoo24 <99449600+adoo24@users.noreply.github.com>
2023-05-19 12:44:34 +0900

    Update Main.js

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useNavigate } from "react-router-dom";
 
-import "swiper/css";
+// import "swiper/css";
 import "swiper/swiper.min.css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
