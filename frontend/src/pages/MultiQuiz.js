dc76410fa34b42e808d065fa446f5a2c82b61b2e
Chos1 <chosw0812@naver.com>
2023-05-23 18:00:33 +0900

    [#COD-92][FE/BE] feat : 단체 퀴즈 API연결

--- frontend/src/pages/MultiQuiz.js
+++ frontend/src/pages/MultiQuiz.js
@@ -196,6 +196,155 @@ const ProblemBox = styled.div`
   box-shadow: 2px 2px 2px 2px #dddddd;
 `;
 
+// 모달 창 뒷배경
+const ModalBox = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #00000050;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 25px;
+`;
+
+const InputDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-right: 20px;
+
+  > div:nth-of-type(1) {
+    padding-left: 20px;
+    width: 100px;
+    margin-right: 10px;
+    font-size: 20px;
+  }
+
+  > select {
+    font-family: "NanumSquareNeo-Variable";
+    width: 450px;
+    height: 45px;
+    outline: none;
+    border: solid 2px #aeaeae;
+    border-radius: 8px;
+    padding-left: 10px;
+    padding-right: 50px;
+
+    > option {
+    }
+  }
+
+  > input {
+    padding-left: 10px;
+    padding-right: 10px;
+    font-family: "NanumSquareNeo-Variable";
+    width: 450px;
+    height: 40px;
+    outline: none;
+    border: solid 2px #aeaeae;
+    border-radius: 8px;
+  }
+`;
+
+const ModalContent = styled.div`
+  font-weight: 400;
+  height: 470px;
+  width: 700px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+
+  > div:nth-of-type(1) {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    // 엑스버튼
+    width: 600px;
+    height: 50px;
+    cursor: pointer;
+    font-size: 1.3em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // margin-left: 450px;
+
+    > div:nth-of-type(1) {
+      margin-top: 40px;
+    }
+
+    > div:nth-of-type(2) {
+      color: #aeaeae;
+      font-size: 15px;
+    }
+  }
+
+  > div:nth-of-type(3) {
+    margin-top: 0px;
+    display: flex;
+    flex-direction: column;
+    height: 250px;
+    width: 600px;
+  }
+
+  > div:nth-of-type(4) {
+    margin-top: 10px;
+  }
+
+  > div {
+    display: flex;
+    // justify-content: space-evenly;
+    justify-content: space-around;
+
+    width: 600px;
+    margin-top: 1.25rem;
+    > button:nth-of-type(1) {
+      font-family: "NanumSquareNeo-Variable";
+      color: white;
+      border: none;
+      width: 265px;
+      height: 45px;
+      margin-top: 20px;
+      padding: 0.4rem 0.6rem;
+      border-radius: 5px;
+      font-size: 1.3em;
+      font-weight: 600;
+      background-color: #fab809;
+      cursor: pointer;
+      box-shadow: 3px 2px 5px #fab809;
+    }
+
+    > button:nth-of-type(2) {
+      font-family: "NanumSquareNeo-Variable";
+      color: white;
+      border: none;
+      width: 265px;
+      height: 45px;
+      margin-top: 20px;
+      padding: 0.4rem 0.6rem;
+      border-radius: 5px;
+      font-size: 1.3em;
+      font-weight: 600;
+      background-color: #aeaeae;
+      cursor: pointer;
+      box-shadow: 3px 2px 5px #aeaeae;
+    }
+  }
+`;
+
+const Hr = styled.div`
+  border-top: 1px solid #bcbfc180;
+  height: 8px;
+  margin-top: 20px;
+`;
+
 export default function ProblemSolved() {
   const url = "http://localhost:8080";
   let stompUserClient;
@@ -209,10 +358,35 @@ export default function ProblemSolved() {
     setShowModal(!showModal);
     console.log("modal");
   };
+  // const [stompUserClient, setStompUserClient] = useState("");
+  const [title, setTitle] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const [Cnt, setCnt] = useState("");
+  // const [stompUserClient, setStompUserClient] = useState(
+  //   window.localStorage.getItem("stompUserClient")
+  // );
+
+  function titleChange(e) {
+    setTitle(e.target.value);
+  }
+  function CateChange(e) {
+    setCategoryId(e.target.value);
+  }
+  function CntChange(e) {
+    setCnt(e.target.value);
+  }
 
   const [nickname, setNickname] = useState(
     window.localStorage.getItem("nickname")
   );
+  const [userId, setUserId] = useState(window.localStorage.getItem("userId"));
+  const [data, setData] = useState({
+    room_name: title,
+    user_id: userId,
+    problem_category_id: categoryId,
+    problem_cnt: Cnt,
+  });
+
   const navigate = useNavigate();
   const categories = [
     "ALL",
@@ -239,10 +413,46 @@ export default function ProblemSolved() {
       .catch((e) => console.log(e));
   }
 
+  function sendBroadcast(message) {
+    let username = nickname;
+    stompUserClient.send(
+      "/app/room",
+      {},
+      JSON.stringify({
+        fromLogin: username,
+        message: message,
+        type: "broadcast",
+      })
+    );
+  }
+
+  useEffect(() => {
+    setData({
+      room_name: title,
+      user_id: userId,
+      problem_category_id: categoryId,
+      problem_cnt: Cnt,
+    });
+  }, [title, categoryId, Cnt]);
+
+  function addRoom() {
+    // let roomName = document.getElementById("roomName").value;
+    console.log(data);
+    axios
+      .post(url + "/game/room", data)
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (jqXHR) {
+        console.log(jqXHR);
+      });
+  }
+
   useEffect(() => {
     console.log("connecting to server...");
     let socket = new SockJS(url + "/room");
     stompUserClient = Stomp.over(socket);
+    window.localStorage.setItem("stompUserClient", stompUserClient);
     stompUserClient.connect({}, function (frame) {
       console.log("connected to: " + frame);
       // sendBroadcast(userName + " logined");
@@ -253,15 +463,6 @@ export default function ProblemSolved() {
       });
     });
     fetchAll();
-
-    // api
-    //   .get(`http://localhost:8080/game/roomList`)
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     if (res.status === 200) {
-    //     }
-    //   })
-    //   .catch((e) => console.log(e));
   });
 
   const [selected, setSelected] = useState("ALL");
@@ -515,7 +716,66 @@ export default function ProblemSolved() {
           </ProblemScroll>
         </ProblemBox>
       </RoomDiv>
-      {showModal && <RoomMakeModal clickModal={clickModal} func={submit} />}
+      {showModal && (
+        <ModalBox>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <div>
+              <Title>방 생성하기</Title>
+              <div onClick={clickModal}>X</div>
+            </div>
+            <Hr />
+            <div>
+              <InputDiv>
+                <div>방 제목</div>
+                <input
+                  onChange={titleChange}
+                  placeholder="방 제목을 입력해 주세요."
+                ></input>
+              </InputDiv>
+              <InputDiv>
+                <div>카테고리</div>
+                <select
+                  name="Category"
+                  form="CateForm"
+                  required
+                  defaultValue=""
+                  onChange={CateChange}
+                >
+                  <option value="" disabled hidden>
+                    카테고리를 설정해주세요.
+                  </option>
+                  <option value="1">cate1</option>
+                  <option value="2">cate2</option>
+                  <option value="3">cate3</option>
+                  <option value="4">cate4</option>
+                </select>
+              </InputDiv>
+              <InputDiv>
+                <div>문제 수</div>
+                <select
+                  name="order"
+                  form="num"
+                  required
+                  defaultValue=""
+                  onChange={CntChange}
+                >
+                  <option value="" disabled hidden>
+                    문제 수를 설정해주세요.
+                  </option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                </select>
+              </InputDiv>
+            </div>
+            <div>
+              <button onClick={addRoom}>방 만들기</button>
+              <button onClick={clickModal}>취소</button>
+            </div>
+          </ModalContent>
+        </ModalBox>
+      )}
     </Div>
   );
 }
