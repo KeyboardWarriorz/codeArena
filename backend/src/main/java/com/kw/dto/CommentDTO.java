9a9a19c616f6cb2c88631e8184c701288370ad7d
adoo24 <adoo244475@gmail.com>
2023-05-29 19:01:49 +0900

    [#COD-126][BE] feat: 댓글에 티어 보이게 함

--- backend/src/main/java/com/kw/dto/CommentDTO.java
+++ backend/src/main/java/com/kw/dto/CommentDTO.java
@@ -3,17 +3,19 @@ package com.kw.dto;
 import com.kw.entity.Comment;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Getter
+@Setter
 public class CommentDTO {
 	private Long commentId;
 	private String content;
 	private String nickName;
 	private String profile_image;
 	private String user_id;
-
+	private String tier;
 	
 	public CommentDTO(Comment comment) {
 		this.commentId = comment.getCommentId();
